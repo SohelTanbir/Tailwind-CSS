eb37580c5fd11fb1d7757243f5d193a34f62d12c
SohelTanbir <soheltanbir19@gmail.com>
2023-07-23 23:38:41 +0600

    project design is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 const humbergerIcon = document.getElementById("humberger-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const body = document.querySelector("body");
+
+
 humbergerIcon.addEventListener("click", () =>{
     humbergerIcon.classList.toggle("open");
+    mobileMenu.classList.toggle("hide");
+    // disabled scroll when humberger menu is opened
+    body.style.overflow = body.style.overflow === 'hidden' ? 'auto' : 'hidden';
 })
+
